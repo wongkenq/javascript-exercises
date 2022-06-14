723d766f4059e19d93e03802491ffc1e6585a3a8
KenWong <wongkenq@gmail.com>
2022-06-14 16:43:30 -0700

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    let arr = args[0]
+    return arr.filter(x => !args.includes(x))
 };
 
 // Do not edit below this line
