78aa2a03400852605c1db1d744ab558047e462b0
KenWong <wongkenq@gmail.com>
2022-06-14 23:48:07 -0700

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) return 0
+  else {
+    return arr.reduce((a,b) => a+b, 0)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((a,b) => a*b, 1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1
+
+  for (let i = 2; i <= num; i++){
+    result *= i
+  }
+
+  return result
 };
 
 // Do not edit below this line
