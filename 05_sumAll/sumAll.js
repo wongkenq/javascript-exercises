102995500798dc0046aa0c7a87489c27f94c98ac
KenWong <wongkenq@gmail.com>
2022-06-14 23:20:48 -0700

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let small;
+  let big;
+  let sum;
 
+  if (end < 0 || start < 0) {
+    return "ERROR";
+  } else if (typeof end !== "number" || typeof start !== "number") {
+    return "ERROR";
+  } else if (start > end) {
+    small = end;
+    big = start;
+    sum = small;
+  } else if (end > start) {
+    small = start;
+    big = end;
+    sum = small;
+  }
+
+  for (let i = small + 1; i <= big; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
