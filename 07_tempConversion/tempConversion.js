0d7d2e83def42460aa7accd6cfbd2718880ad3c8
KenWong <wongkenq@gmail.com>
2022-06-14 23:32:08 -0700

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return +((temp - 32) * 5/9).toFixed(1)
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return +(temp * 9/5 + 32).toFixed(1)
 };
 
 // Do not edit below this line
