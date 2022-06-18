af2a7c90cef53be8427e1d1392c9038e08b75dcc
KenWong <wongkenq@gmail.com>
2022-06-18 11:29:45 -0700

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let punctuation = ',.?! '
+  
+    str = str.toLowerCase()
 
-};
+    return (
+        str
+            .split('')
+            .filter(x => !punctuation.includes(x))
+            .reverse()
+            .join('')) 
+        === 
+        str
+            .split('')
+            .filter(x => !punctuation.includes(x))
+            .join('')
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
