6a512eb76eac3d676c83cd39187004c02a54210f
KenWong <wongkenq@gmail.com>
2022-06-18 11:49:19 -0700

    complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let arr = [0, 1];
 
+  if (typeof num === "string") {
+    num = Number(num);
+  }
+  if (num < 0) return "OOPS";
+  else {
+    for (let i = 2; i <= num; i++) {
+      arr[i] = arr[i - 2] + arr[i - 1];
+    }
+
+    return arr[num];
+  }
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
