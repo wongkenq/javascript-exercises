ceba3050d2b21011a5a46e6e44e58b0e932fd1dc
KenWong <wongkenq@gmail.com>
2022-06-19 22:56:44 -0700

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+	return obj.sort((a,b) => {
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+      }, 0)[0]
 };
 
 // Do not edit below this line
